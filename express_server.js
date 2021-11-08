9ad6da14dc23b4b2d20a40115b8dc4e9dd834f85
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-11-08 21:37:03 +0000

    Created functionality so that newly generated tiny url will redirect to corresponding long url.

--- express_server.js
+++ express_server.js
@@ -28,13 +28,20 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL
+  urlDatabase[shortURL] = longURL
+  res.redirect(`/urls/${shortURL}`);
 })
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+})
+
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 })
 
 
