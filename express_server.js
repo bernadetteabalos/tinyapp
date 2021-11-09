e658cdb0ecb5a2f5df06d0d5e9fae36f165b0d58
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-11-09 19:55:48 +0000

    Added functionality for login and logout as well as cookies.

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+var cookieParser = require('cookie-parser')
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 const { response } = require("express");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,12 +22,13 @@ app.get("/urls.json", (req, res) => {
 })
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] }
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -36,13 +39,14 @@ app.post("/urls", (req, res) => {
 })
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 })
 //When clicking short url, redirect to long url
 app.get('/u/:shortURL', (req, res) => {
+  const templateVars = { username: req.cookies["username"] }
   const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
+  res.redirect(longURL), templateVars;
 })
 //Delete
 app.post('/urls/:shortURL/delete', (req, res) => {
@@ -55,6 +59,20 @@ app.post('/urls/:shortURL', (req, res) => {
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL
   res.redirect(`/urls/${shortURL}`);
+})
+
+//Login
+app.post('/login', (req, res) => {
+  const username = req.body
+  res.cookie('username', username);
+  res.redirect('/urls');
+})
+
+//Logout
+app.post('/logout', (req, res) => {
+  const username = req.body
+  res.clearCookie('username', username);
+  res.redirect('/urls');
 })
 
 app.listen(PORT, () => {
